74d4ae2d243d212a06d9c434e0abff29fc44a734
root <to.zawistowski@gmail.com>
2019-04-07 00:49:05 +0200

    Working commands

--- Documents/SEMESTR_4/SO/TZ_SHELL/shellix.c
+++ Documents/SEMESTR_4/SO/TZ_SHELL/shellix.c
@@ -6,6 +6,10 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h> 
+#include<sys/wait.h> 
+
 
 void hello(){
 
@@ -13,41 +17,74 @@ printf("\n\tWelcome to shellix shell stranger\n");
 
 }
 
+
 int readTask(char *task){
 
     char* buf; 
     buf = readline("\nsx>> "); /* Czytanie do bufera */
     if (strlen(buf) != 0) { 
         add_history(buf);
-        strcpy(task, buf);     /* Funkcja kończąc działanie zmieni "task" */       
+        strcpy(task, buf);     /* Funkcja kończąc działanie zmieni "task" */     
 	return 0; 
-    } else { 
+    } else {
+	perror("Problem with reading your task");
         return 1; 
     } 
 }   
 
-void clearScreen()
-{
-  const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
-  write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+void clearScreen(){
+    const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+    write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+}
+
+/*
+GETOPT może się przydać do poleceń pwd lub cd
+*/
+
+void parseSpace(char *task, char* parsed[]){
+    int count=0;
+    char *duplicate = strdup(task);	// Dzięki temu nie tracimy dostępu do oryginalnej linii wejściowej
+    char *word;
+    while((parsed[count]=strsep(&duplicate," ")) != NULL){
+	count++;
+    }
 }
 
 
+void execTask(char** arg_list){
+ 
+  pid_t child_pid; 
 
+  child_pid = fork (); 
+
+
+  if (child_pid == 0) {
+	execvp (arg_list[0],  arg_list); 
+    	exit(0);
+  }
+  
+  else {
+	wait(NULL);  	
+	return;
+  }
+
+} 
 
 
 int main(){
 
     char input[12];
     char exit[] = "exit";
-
+    char *parsed[100];	/* Tablica parametrów linii wejściowej, parsed[0] - nazwa programu */
 
     clearScreen();
     hello();
 
     while(strcmp(input,exit)!=0){
 	readTask(input);
-        //sleep(1);
+	parseSpace(input,parsed);
+	execTask(parsed); 
+        
 
 }
 
